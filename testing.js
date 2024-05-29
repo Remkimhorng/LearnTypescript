032cd6525fece40a996877027b901f5033f6321a
Horng <kimhongrem@gmail.com>
2024-05-29 08:56:32 +0700

    add foldeer typeScript

--- testing.js
+++
@@ -1,8 +0,0 @@
-// const names: string[] = ["Dylan"];
-// names.push("Jack");
-// console.log(names);
-var number = [1, 2, 3];
-number.push(7);
-console.log(number);
-var head = number[3];
-console.log(head);
---
+++ typescriptTS/testing.ts
@@ -0,0 +1,11 @@
+// const names: string[] = ["Dylan"];
+
+// names.push("Jack");
+// console.log(names);
+const number1 = [1, 2, 3];
+number1.push(7);
+
+console.log(number1);
+
+let head1: number = number1[3];
+console.log(head1);
